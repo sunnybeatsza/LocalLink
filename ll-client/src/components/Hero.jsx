2b4616072158368ca033df80dbe0d6088fdbf43e
momakgejhb024 <momakgejhb024@student.wethinkcode.co.za>
2024-10-26 13:07:06 +0200

    New changes

--- ll-client/src/components/Hero.jsx
+++ ll-client/src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ImgHero from "../Assets/pinwheels-6535599_1280.jpg";
-import LogoImg from "../Assets/Logo_Green2-removebg-preview.png";
+import LogoImg from "../Assets/Logo_Green2-removebg-Edited.png";
 import "./Hero.css";
 
 export const Hero = () => {
@@ -8,13 +8,16 @@ export const Hero = () => {
     <div className="hero" style={{ backgroundImage: `url(${ImgHero})` }}>
       <div className="content">
         <div className="content-text">
-          <h1>Kindess to Nature. Kindness to communities</h1>
+          <h1>
+            Kindness to Nature. <br />
+            Kindness to communities.
+          </h1>
           <p>Our local food bank promise</p>
         </div>
 
         <div className="content-buttons">
           <h2 id="welcome-text">Welcome to</h2>
-          <img src={LogoImg} alt="" id="Logo-image" />
+          <img src={LogoImg} alt="Logo" id="Logo-image" />
           <button>
             <a href="">LOGIN</a>
           </button>
